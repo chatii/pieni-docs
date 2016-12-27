960ea970ba3bd636c8f67eb84040eccbd32edda6
Kenta Saito <kenta.saito@sagittar.org>
2016-12-28 00:50:09 +0900

    2016-12-28 00:50:09

--- views/docs/download.php
+++ views/docs/download.php
@@ -1,18 +1,33 @@
 <h1>ダウンロード</h1>
 
 <h2>GitHub</h2>
-<a href="https://github.com/sagittar-org/pieni">https://github.com/sagittar-org/pieni</a>
+<p>
+パッケージごとに以下のリポジトリがあります。<br>
+個別のパッケージが必要な場合以外は後述のインストール方法が便利です。<br>
+</p>
+
+<ul>
+<li><a href="https://github.com/sagittar-org/pieni-base">sagittar-org/pieni-base</a></li>
+<li><a href="https://github.com/sagittar-org/pieni-misc">sagittar-org/pieni-misc</a></li>
+<li><a href="https://github.com/sagittar-org/pieni-crud">sagittar-org/pieni-crud</a></li>
+<li><a href="https://github.com/sagittar-org/pieni-docs">sagittar-org/pieni-docs</a></li>
+<li><a href="https://github.com/sagittar-org/pieni">sagittar-org/pieni</a></li>
+</ul>
 
 <h2>インストール方法</h2>
+<p>全てのパッケージをまとめてインストールするには以下を実行。</p>
+<pre>
+composer require sagittar-org/pieni
+</pre>
 <p>
 Apacheのmod_rewriteが有効なら何も行うことなく、このドキュメンテーションが表示されるでしょう。<br>
 デモを試すには以下を実行。<br>
 <pre>
 echo 'CREATE DATABASE `pieni_crud`;' | mysql
-mysql pieni_crud < crud/misc/pieni_crud.dump 
+mysql pieni_crud < vendor/sagittar-org/pieni-crud/misc/pieni_crud.dump
 </pre>
 </p>
 <p>
-/package_list.phpに<code>define('APPPATH', FCPATH.'application/');</code>と定義されています。<br>
-/applicationディレクトリを作成し、いつでもハックをスタートできます!<br>
+index.phpに<code>define('APPPATH', FCPATH);</code>と定義されています。<br>
+index.phpと同じ階層にcontrollersディレクトリやviewsディレクトリを作成し、いつでもハックをスタートできます!<br>
 </p>
