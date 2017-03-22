6587079be4048dff711587e7292098b6ec6011e3
Kenta Saito <kenta.saito@sagittar.org>
2017-03-23 00:32:34 +0900

    2017-03-23 00:32:34

--- config/core.php
+++ config/core.php
@@ -17,4 +17,10 @@ $config = (object) [
 		'item',
 	],
 	'default_target' => 'docs',
+
+	'session_save_path' => session_save_path(),
+	'session_gc_maxlifetime' => ini_get('session.gc_maxlifetime'),
+	'session_cookie_lifetime' => ini_get('session.cookie_lifetime'),
+	'session_gc_probability' => ini_get('session.gc_probability'),
+	'session_gc_divisor' => ini_get('session.gc_divisor'),
 ];
